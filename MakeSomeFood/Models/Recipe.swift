18afdf7bec6d2077b7337a28e00368f5235bf634
Aleksandr Kretov <kretov.1988@gmail.com>
2023-02-27 23:48:39 +0500

    Recipe model refactoring

--- MakeSomeFood/Models/Recipe.swift
+++ MakeSomeFood/Models/Recipe.swift
@@ -6,17 +6,17 @@
 //
 import Foundation
 
-protocol PropertieLoopable {
+protocol PropertyLoopable {
     func allProperties() -> [String: Any]
 }
 
-extension PropertieLoopable {
+extension PropertyLoopable {
     func allProperties() -> [String: Any] {
         var result: [String: Any] = [:]
         let mirror = Mirror(reflecting: self)
         guard let style = mirror.displayStyle, style == .struct || style == .class else { return result }
         for (property, value) in mirror.children {
-            guard let property = property else {
+            guard let property else {
                 continue
             }
             result[property] = value
@@ -28,19 +28,19 @@ extension PropertieLoopable {
 // MARK: - RecipeResponse
 
 struct RecipeResponse: Codable {
-    let from: Int?
-    let to: Int?
-    let count: Int?
-    let links: Links?
+    let from: Int
+    let to: Int
+    let count: Int
+    let links: Links
     let hits: [Hit]
 
-        enum CodingKeys: String, CodingKey {
-            case from = "from"
-            case to = "to"
-            case count = "count"
-            case links = "_links"
-            case hits = "hits"
-        }
+    enum CodingKeys: String, CodingKey {
+        case from
+        case to
+        case count
+        case links = "_links"
+        case hits
+    }
 }
 
 // MARK: - Hit
@@ -67,123 +67,51 @@ struct Next: Codable {
 // MARK: - Recipe
 
 struct Recipe: Codable {
-    let uri: String?
     let label: String
     let image: String
-    let images: Images?
-    let source: String?
-    let url: String?
-    let shareAs: String?
-    let yield: Double?
-    let dietLabels: [String]?
-    let healthLabels: [String]?
-    let cautions: [String]?
-    let ingredientLines: [String]?
-    let ingredients: [Ingredient]?
-    let calories: Double?
-    let totalWeight: Double?
-    let totalTime: Double?
-    let cuisineType: [String]?
-    let dishType: [String]?
-    let totalNutrients: TotalNutrients?
-    let totalDaily: TotalNutrients?
-    let glycemicIndex: Int?
-    let digest: [Digest]?
-    let mealType: [String]?
+    let images: Images
+    let source: String
+    let url: String
+    let yield: Double
+    let dietLabels: [String]
+    let healthLabels: [String]
+    let cautions: [String]
+    let ingredients: [Ingredient]
+    let calories: Double
+    let totalWeight: Double
+    let totalTime: Double
+    let cuisineType: [String]
+    let dishType: [String]
+    let totalNutrients: TotalNutrients
+    let totalDaily: TotalNutrients
+    let digest: [Digest]
+    let mealType: [String]
 }
 
-struct TotalNutrients: Codable, PropertieLoopable {
-//    let enercKcal: Nutrient?
-    let fat: Nutrient?
-//    let fasat: Nutrient?
-//    let fatrn: Nutrient?
-//    let fams: Nutrient?
-//    let fapu: Nutrient?
-    let chocdf: Nutrient?
-//    let chocdfNet: Nutrient?
+struct TotalNutrients: Codable, PropertyLoopable {
+
     let fibtg: Nutrient?
-//    let sugar: Nutrient?
-//    let sugarAdded: Nutrient?
-    let procnt: Nutrient?
     let chole: Nutrient?
-    let na: Nutrient?
-    let ca: Nutrient?
-    let mg: Nutrient?
-    let k: Nutrient?
-//    let fe: Nutrient?
-//    let zn: Nutrient?
-//    let p: Nutrient?
-//    let vitaRae: Nutrient?
-    let vitc: Nutrient?
-//    let thia: Nutrient?
-//    let ribf: Nutrient?
-//    let nia: Nutrient?
-    let vitb6A: Nutrient?
-//    let foldfe: Nutrient?
-//    let folfd: Nutrient?
-//    let folac: Nutrient?
-    let vitb12: Nutrient?
-    let vitd: Nutrient?
-//    let tocpha: Nutrient?
-//    let vitk1: Nutrient?
-//    let sugarAlcohol: Nutrient?
-//    let water: Nutrient?
 
     enum CodingKeys: String, CodingKey {
-//        case enercKcal = "ENERC_KCAL"
-        case fat = "FAT"
-//        case fasat = "FASAT"
-//        case fatrn = "FATRN"
-//        case fams = "FAMS"
-//        case fapu = "FAPU"
-        case chocdf = "CHOCDF"
-//        case chocdfNet = "CHOCDF.net"
-        case fibtg = "FIBTG"
-//        case sugar = "SUGAR"
-//        case sugarAdded = "SUGAR.added"
-        case procnt = "PROCNT"
         case chole = "CHOLE"
-        case na = "NA"
-        case ca = "CA"
-        case mg = "MG"
-        case k = "K"
-//        case fe = "FE"
-//        case zn = "ZN"
-//        case p = "P"
-//        case vitaRae = "VITA_RAE"
-        case vitc = "VITC"
-//        case thia = "THIA"
-//        case ribf = "RIBF"
-//        case nia = "NIA"
-        case vitb6A = "VITB6A"
-//        case foldfe = "FOLDFE"
-//        case folfd = "FOLFD"
-//        case folac = "FOLAC"
-        case vitb12 = "VITB12"
-        case vitd = "VITD"
-//        case tocpha = "TOCPHA"
-//        case vitk1 = "VITK1"
-//        case sugarAlcohol = "Sugar.alcohol"
-//        case water = "WATER"
+        case fibtg = "FIBTG"
     }
 }
 
 struct Nutrient: Codable, Equatable {
-    let label: String?
-    let quantity: Double?
-    let unit: String?
-} 
+    let label: String
+    let quantity: Double
+    let unit: String
+}
 
 // MARK: - Digest
 
 struct Digest: Codable {
-    let label: String?
-    let tag: String?
-    let schemaOrgTag: String?
-    let total: Double?
-    let hasRdi: Bool?
-    let daily: Double?
-    let unit: String?
+    let label: String
+    let total: Double
+    let daily: Double
+    let unit: String
 }
 
 // MARK: - Images
@@ -198,20 +126,18 @@ struct Images: Codable {
 // MARK: - Large
 
 struct ImageSize: Codable {
-    let url: String?
-    let width: Int?
-    let height: Int?
+    let url: String
 }
 
 // MARK: - Ingredient
 
-struct Ingredient: Codable {
-    let text: String?
-    let quantity: Double?
+struct Ingredient: Codable, Equatable {
+    let text: String
+    let quantity: Double
     let measure: String?
-    let food: String?
-    let weight: Double?
+    let food: String
+    let weight: Double
     let foodCategory: String?
-    let foodId: String?
+    let foodId: String
     let image: String?
 }
