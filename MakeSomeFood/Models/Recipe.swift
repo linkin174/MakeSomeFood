4ee575d16e504327d10c7dbea4c0f038a586b6a2
Aleksandr Kretov <kretov.1988@gmail.com>
2023-03-06 20:03:15 +0500

    Recipe model made with all fields optional. Property Iterable protocol added

--- MakeSomeFood/Models/Recipe.swift
+++ MakeSomeFood/Models/Recipe.swift
@@ -6,6 +6,27 @@
 //
 import Foundation
 
+protocol PropertyIterable {
+    func allProperties() -> [String: Any]
+}
+
+extension PropertyIterable {
+    func allProperties() -> [String: Any] {
+        var result: [String: Any] = [:]
+        let mirror = Mirror(reflecting: self)
+        guard let style = mirror.displayStyle, style == .struct || style == .class else {
+           return result
+        }
+        for (label, value) in mirror.children {
+            guard let label else {
+                continue
+            }
+            result[label] = value
+        }
+        return result
+    }
+}
+
 // MARK: - RecipeResponse
 
 struct RecipeResponse: Codable {
@@ -27,7 +48,7 @@ struct RecipeResponse: Codable {
 // MARK: - Hit
 
 struct Hit: Codable {
-    let recipe: Recipe
+    let recipe: Recipe?
 //    let links: Links?
 }
 
@@ -46,36 +67,36 @@ struct Next: Codable {
 }
 
 // MARK: - Recipe
+
 struct Recipe: Codable, Equatable {
     let label: String?
     let image: String?
     let images: Images?
     let source: String?
     let url: String?
-    let uri: String
+    let uri: String?
     let shareAs: String?
     let yield: Double?
-    let dietLabels: [String]
-    let healthLabels: [String]
-    let cautions: [String]
-    let ingredients: [Ingredient]
+    let dietLabels: [String]?
+    let healthLabels: [String]?
+    let cautions: [String]?
+    let ingredients: [Ingredient]?
     let calories: Double?
     let totalWeight: Double?
     let totalTime: Double?
-    let cuisineType: [String]
-    let dishType: [String]
+    let cuisineType: [String]?
+    let dishType: [String]?
     let totalNutrients: TotalNutrients?
     let totalDaily: TotalNutrients?
-    let digest: [Digest]
-    let mealType: [String]
-    
+    let digest: [Digest]?
+    let mealType: [String]?
+
     static func == (lhs: Recipe, rhs: Recipe) -> Bool {
         lhs.shareAs == rhs.shareAs
     }
 }
 
 struct TotalNutrients: Codable {
-
     let fibtg: Nutrient?
     let chole: Nutrient?
 
@@ -86,44 +107,53 @@ struct TotalNutrients: Codable {
 }
 
 struct Nutrient: Codable, Equatable {
-    let label: String
-    let quantity: Double
-    let unit: String
+    let label: String?
+    let quantity: Double?
+    let unit: String?
 }
 
 // MARK: - Digest
 
 struct Digest: Codable {
-    let label: String
-    let total: Double
-    let daily: Double
-    let unit: String
+    let label: String?
+    let total: Double?
+    let daily: Double?
+    let unit: String?
 }
 
 // MARK: - Images
 
-struct Images: Codable {
+struct Images: Codable, PropertyIterable {
     let thumbnail: ImageSize?
     let small: ImageSize?
     let regular: ImageSize?
     let large: ImageSize?
+
+    enum CodingKeys: String, CodingKey {
+        case thumbnail = "THUMBNAIL"
+        case small = "SMALL"
+        case regular = "REGULAR"
+        case large = "LARGE"
+    }
 }
 
 // MARK: - Large
 
 struct ImageSize: Codable {
-    let url: String
+    let url: String?
+    let width: Double?
+    let height: Double?
 }
 
 // MARK: - Ingredient
 
 struct Ingredient: Codable, Equatable {
-    let text: String
-    let quantity: Double
+    let text: String?
+    let quantity: Double?
     let measure: String?
-    let food: String
-    let weight: Double
+    let food: String?
+    let weight: Double?
     let foodCategory: String?
-    let foodId: String
+    let foodId: String?
     let image: String?
 }
